c91a77f304c8b97abb969f4dd7bda34ff48cf227
jmereszko <137088739+jmereszko@users.noreply.github.com>
2025-09-09 13:22:05 +1000

    Update installDependencies.sh

--- mern-social-k8s/installDependencies.sh
+++ mern-social-k8s/installDependencies.sh
@@ -7,11 +7,11 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt update
 sudo apt install docker-ce
 
-sudo usermod -aG docker ubuntu
-newgrp docker
-
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+sudo usermod -aG docker ubuntu
+newgrp docker
