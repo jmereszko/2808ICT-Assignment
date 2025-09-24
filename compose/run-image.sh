eb9324f1415c36b87b5a38e18953d9add32e45f2
Madi Abio <madelineabio@gmail.com>
2025-09-24 18:37:55 +1000

    Fixed run image cmd hopefully

--- compose/run-image.sh
+++ compose/run-image.sh
@@ -1,5 +1,9 @@
 # Run ZAP image on the same network
-docker rm -f zap # Remove existing zap containers
+docker rm -f zap
 docker run -u zap --name zap -p 8080:8080 \
   --network compose_frontend \
-  -d ghcr.io/zaproxy/zaproxy:stable zap.sh -daemon -port 8080 -host 0.0.0.0 -config api.key=MySecretKey123
+  -d ghcr.io/zaproxy/zaproxy:stable \
+  zap.sh -daemon -port 8080 -host 0.0.0.0 \
+  -config api.key=MySecretKey123 \
+  -config api.addrs.addr.name=.* \
+  -config api.addrs.addr.regex=true
